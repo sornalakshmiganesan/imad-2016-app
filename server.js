cc94882891568abc823f130d97a89521e01230d9
sornalakshmiganesan <sorna.uma20@gmail.com>
2016-09-27 19:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('article-one requested willbe served')
-}
+    res.sendfile('article-one requested will be served')
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
